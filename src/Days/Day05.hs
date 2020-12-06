0a88bacdd632e60018d2257673ebd385e692eb89
Shaun Lee <shaun.lee@gmail.com>
2020-12-05 23:25:51 -0800

    Day 5 complete.

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -7,6 +7,7 @@ import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Text (Text)
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
@@ -21,19 +22,33 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy1 boardingPass space
+
+boardingPass :: Parser BoardingPass
+boardingPass = count 10 anyChar
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [BoardingPass]
+type BoardingPass = String
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = maximum . map computeBP
+
+computeBP :: BoardingPass -> Int
+computeBP bp
+    = let ( rowBSP, colBSP ) = splitAt 7 bp
+          row = foldl' (\acc c -> acc * 2 + if c == 'F' then 0 else 1) 0 rowBSP
+          col = foldl' (\acc c -> acc * 2 + if c == 'L' then 0 else 1) 0 colBSP
+    in row * 8 + col
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB bps = head $ [mn..mx] \\ takenSeats
+  where takenSeats = sort $ map computeBP bps
+        mn = head takenSeats
+        mx = last takenSeats
