4cce602c1830337d02a960317f27695e58699db2
Shaun Lee <shaun.lee@gmail.com>
2020-12-06 23:00:54 -0800

    Day 7 complete.

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -21,19 +21,70 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = Map.fromList <$> sepBy1 bagRule (char '\n')
+
+bagRule :: Parser BagRule
+bagRule = do
+  label <- parseLabel
+  string " bags contain "
+  contents <- parseContents
+  char '.'
+  return (label, contents)
+
+parseLabel :: Parser String
+parseLabel = do
+  w1 <- many1 letter
+  string " "
+  w2 <- many1 letter
+  return $ w1 ++ " " ++ w2
+
+parseContents :: Parser (Map String Int)
+parseContents =
+  choice
+    [ do
+        string "no other bags"
+        return Map.empty,
+      Map.fromList
+        <$> sepBy1
+          ( do
+              cnt <- decimal
+              char ' '
+              label <- parseLabel
+              if cnt == 1 then string " bag" else string " bags"
+              return (label, cnt)
+          )
+          (string ", ")
+    ]
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Map String (Map String Int)
 
-type OutputA = Void
+type BagRule = (String, Map String Int)
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA rulesMap = go ["shiny gold"] Set.empty
+  where
+    bagContents = Map.map Map.keysSet rulesMap
+    go [] acc = length acc
+    go (k : ks) acc =
+      let containing = Map.keys $ Map.filter (Set.member k) bagContents
+       in go (ks ++ containing) (Set.union acc (Set.fromList containing))
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB rulesMap = go [("shiny gold", 1)] - 1
+  where
+    go =
+      sum
+        . map
+          ( \(k, n) ->
+              n
+                * if Map.null $ rulesMap Map.! k
+                  then 1
+                  else 1 + go (Map.toAscList $ rulesMap Map.! k)
+          )
