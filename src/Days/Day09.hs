f07dacb2c486f7e4cec113751d24b4eb7687949e
Shaun Lee <shaun.lee@gmail.com>
2020-12-08 22:05:07 -0800

    Day 9 complete with better data structure.

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -2,11 +2,14 @@ module Days.Day09 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
+import Data.Foldable (toList)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
@@ -21,10 +24,10 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = sepBy decimal (char '\n')
+inputParser = Seq.fromList <$> sepBy decimal (char '\n')
 
 ------------ TYPES ------------
-type Input = [Int]
+type Input = Seq Int
 
 type OutputA = Int
 
@@ -34,20 +37,21 @@ type OutputB = Maybe Int
 windowSize = 25
 
 partA :: Input -> Int
-partA = go . splitAt windowSize
+partA = go . Seq.splitAt windowSize
   where
-    go (_, []) = -1
-    go (pre, x : xs) = let pairs = [(a,b) | a <- pre, b <- tail pre, a /= b]
-                       in if all (\(a, b) -> a + b /= x) pairs
-                          then x
-                          else go (tail pre ++ [x], xs)
+    go (_, Seq.Empty) = -1
+    go (pre@(p Seq.:<| ps), x Seq.:<| xs) =
+      let pairs = [(a,b) | a <- toList pre, b <- toList pre, a /= b]
+      in if all (\(a, b) -> a + b /= x) pairs
+         then x
+         else go (ps Seq.|> x, xs)
 
 ------------ PART B ------------
-overlappingChunksOf :: Int -> [a] -> [[a]]
-overlappingChunksOf k xs =
+overlappingChunksOf :: Int -> Seq a -> [Seq a]
+overlappingChunksOf k xs@(_ Seq.:<| xst) =
   if k > length xs
   then []
-  else Data.List.take k xs : overlappingChunksOf k (tail xs)
+  else Seq.take k xs : overlappingChunksOf k xst
 
 partB :: Input -> OutputB
 partB input = foldr go Nothing [3 .. length input]
