e0a4f599ec4e5a10d2aa553df4e6ce88eb6eaece
Shaun Lee <shaun.lee@gmail.com>
2020-12-14 22:51:07 -0800

    Day 15 complete.

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -2,8 +2,8 @@ module Days.Day15 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as Map
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -14,6 +14,7 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+import Debug.Trace (trace)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +22,41 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy decimal (char ',')
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input =
+  head $
+    Data.List.last $
+      Data.List.take (2020 - length input + 1) $
+        iterate takeTurn (reverse input)
+
+takeTurn :: [Int] -> [Int]
+takeTurn (x : xs) = case elemIndex x xs of
+  Nothing -> 0 : x : xs
+  Just i -> i + 1 : x : xs
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input =
+  snd $
+    foldl'
+      takeTurnB
+      (seen, lst)
+      [cnt .. 30000000 - 1]
+  where
+    cnt = length input
+    (front, [lst]) = splitAt (cnt - 1) input
+    seen = Map.fromList $ zip front [1 ..]
+
+takeTurnB (sn, l) i = case sn Map.!? l of
+  Nothing -> (Map.insert l i sn, 0)
+  Just p -> (Map.insert l i sn, i - p)
