d354abf08da88c33eb96e78735546aeaa5632374
Shaun Lee <shaun.lee@gmail.com>
2020-12-24 23:33:45 -0800

    Day 24 complete.

--- src/Days/Day24.hs
+++ src/Days/Day24.hs
@@ -21,19 +21,114 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy1 tile (char '\n')
+
+tile :: Parser [Dir]
+tile = many1 dir
+
+dir :: Parser Dir
+dir =
+  choice
+    [ E <$ string "e",
+      SE <$ string "se",
+      SW <$ string "sw",
+      W <$ string "w",
+      NW <$ string "nw",
+      NE <$ string "ne"
+    ]
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Tile]
 
-type OutputA = Void
+type Tile = [Dir]
 
-type OutputB = Void
+data Dir = E | SE | SW | W | NW | NE
+  deriving (Eq, Show)
+
+newtype Pos = Pos (Int, Int, Int)
+  deriving (Eq, Ord, Show)
+
+instance Semigroup Pos where
+  (Pos (x1, y1, z1)) <> (Pos (x2, y2, z2)) = Pos (x1 + x2, y1 + y2, z1 + z2)
+
+instance Monoid Pos where
+  mempty = Pos (0, 0, 0)
+
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = length $ Map.filter odd $ initialState input
+
+initialState :: Input -> Map Pos Int
+initialState = foldr (\t -> Map.insertWith (+) (tileToPos t) 1) Map.empty
+
+tileToPos :: Tile -> Pos
+tileToPos = mconcat . map dirToPos
+
+dirToPos :: Dir -> Pos
+dirToPos W = Pos (-1, 1, 0)
+dirToPos SW = Pos (-1, 0, 1)
+dirToPos SE = Pos (0, -1, 1)
+dirToPos E = Pos (1, -1, 0)
+dirToPos NE = Pos (1, 0, -1)
+dirToPos NW = Pos (0, 1, -1)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = map countBlackTiles (iterate oneDay $ initialState input) !! 100
+
+countBlackTiles :: Map Pos Int -> Int
+countBlackTiles = length . Map.filter odd
+
+oneDay :: Map Pos Int -> Map Pos Int
+oneDay cur =
+  let bt = blackTiles cur
+      wt = Set.fromList $ concatMap neighbors bt
+      blackFlipped =
+        foldr
+          ( \p m ->
+              let bn = countBlackNeighbors cur p
+               in Map.insertWith
+                    (+)
+                    p
+                    (if bn == 0 || bn >= 2 then 1 else 0)
+                    m
+          )
+          cur
+          bt
+   in foldr
+        ( \p m ->
+            let bn = countBlackNeighbors cur p
+             in Map.insertWith (+) p (if bn == 2 then 1 else 0) m
+        )
+        blackFlipped
+        wt
+
+countBlackNeighbors :: Map Pos Int -> Pos -> Int
+countBlackNeighbors m p =
+  length $
+    filter odd $
+      map
+        (\k -> Map.findWithDefault 0 k m)
+        (neighbors p)
+
+neighbors :: Pos -> [Pos]
+neighbors p =
+  map
+    (p <>)
+    [ Pos (-1, 1, 0),
+      Pos (-1, 0, 1),
+      Pos (0, -1, 1),
+      Pos (1, -1, 0),
+      Pos (1, 0, -1),
+      Pos (0, 1, -1)
+    ]
+
+blackTiles :: Map Pos Int -> [Pos]
+blackTiles = Map.keys . Map.filter odd
+
+dToP :: Dir -> Pos
+dToP d = _
